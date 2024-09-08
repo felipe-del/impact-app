6154299732c5b59c0a2993784b0b13c85a526cb5
Isaac Felipe Brenes Calderón <isaacfelibrenes1904@gmail.com>
2024-09-08 01:34:05 -0600

    Add SupplierForm and AssetForm components using StandardForm
    - Created `SupplierForm` component for managing supplier data with fields for name, phone, email, and address.
    - Created `AssetForm` component for managing asset data with fields for purchase date, value, responsible ID, supplier ID, category ID, and brand ID.
    - Both forms utilize the `StandardForm` component for consistent form layout and styling.
    - Implemented form submission with error handling and status updates.

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from 'react-router-dom';
 import About from '../pages/about/about.jsx';
 import App from '../App.jsx';
 import Authentication from '../pages/auth/authentication.jsx';
+import SupplierForm from '../pages/supplierForm/supplierForm.jsx';
+import AssetForm from '../pages/assetForm/assetForm.jsx';
 
 export const Routes = createBrowserRouter([
     {
@@ -20,6 +22,14 @@ export const Routes = createBrowserRouter([
                 path: 'about',
                 element: <About/>
             },
+            {
+                path: 'createSupplier',
+                element: <SupplierForm/>
+            },
+            {
+                path: 'createAsset',
+                element: <AssetForm/>
+            }
         ]
     }
 ]);
