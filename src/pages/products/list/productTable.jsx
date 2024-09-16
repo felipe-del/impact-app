89eca989a2b54380d4c5eb48d389620f8ae32d8b
JoF00L <131736806+JoF00L@users.noreply.github.com>
2024-09-15 21:33:08 -0600

    feat-refactor (product-table): - productTable.jsx is now located in ../pages/commonSpace/list/spaceTable.jsx. - Added extra attributes to the table to better reflect the space object.

--- src/pages/products/list/productTable.jsx
+++ src/pages/products/list/productTable.jsx
@@ -61,7 +61,7 @@ const ProductTable = () => {
         <div>
             <div className="d-flex justify-content-end mb-4">
                 <Link to="/app/inventory" className="btn btn-primary me-2">Inventario</Link>
-                <Link to="/nueva-pagina-2" className="btn btn-primary me-2">Registrar producto</Link>
+                <Link to="/productRegister" className="btn btn-primary me-2">Registrar producto</Link>
             </div>
             <h2 className="mb-4">Listado de productos</h2>
             <div className="d-flex justify-content-between align-items-center mb-3">
