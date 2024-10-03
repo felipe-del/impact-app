904c946181095e691b2303e708efa856c9f9f5de
JoF00L <131736806+JoF00L@users.noreply.github.com>
2024-10-02 22:58:44 -0600

    refactor (add-building.jsx): The addBuildingLocation.css import has been removed. Also some visual updates.

--- src/pages/commonSpace/buildingLocation/addBuildingLocation.jsx
+++ src/pages/commonSpace/buildingLocation/addBuildingLocation.jsx
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import { Button, Alert } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import './addBuildingLocation.css';
 import { usePage } from '../../../context/pageContext';
 
 const AddBuildingLocation = () => {
@@ -79,18 +78,20 @@ const AddBuildingLocation = () => {
                 </ol>
             </nav>
             <div className="mt-5 d-flex justify-content-center">
-                <div className="card p-5 shadow-lg" style={{maxWidth: "700px", borderRadius: "10px"}}>
+                <div className="card p-5 shadow-lg" style={{minWidth: "50vw", maxWidth: "70vw", maxHeight: "75vh", borderRadius: "2vh"}}>
                     <h1 className="text-center mb-5">Registrar ubicaciones en edificios</h1>
-                    <div className="button-container text-center mb-4">
-                        <div>
-                            <Button className="btn btn-lg btn-custom btn-space shadow-sm" role="button"
-                                    onClick={handleRegisterBld}>
-                                <i className="fas fa-building"></i> Registro de edificios
-                            </Button>
-                            <Button className="btn btn-lg btn-custom btn-space shadow-sm" role="button"
-                                    onClick={handleRegisterSp}>
-                                <i className="fas fa-map-marker-alt"></i> Registro de espacios
-                            </Button>
+                    <div className="mb-4">
+                        <div className="mb-4 row justify-content-center align-items-center">
+                            <div className="col-sm-6">
+                                <Button className="btn btn-lg btn-custom w-100 shadow-sm" role="button" onClick={handleRegisterBld}>
+                                    <i className="fas fa-building"></i> Registro de edificios
+                                </Button>
+                            </div>
+                            <div className="col-sm-6">
+                                <Button className="btn btn-lg btn-custom w-100 shadow-sm" role="button" onClick={handleRegisterSp}>
+                                    <i className="fas fa-map-marker-alt"></i> Registro de espacios
+                                </Button>
+                            </div>
                         </div>
                     </div>
                     <div className="container2">
@@ -101,7 +102,7 @@ const AddBuildingLocation = () => {
                                     <label htmlFor="buildingId" className="col-sm-4 col-form-label form-label">
                                         <i className="fas fa-building" id="icon-name"></i> Edificio
                                     </label>
-                                    <div className="col-sm-8 w-100 mb-4">
+                                    <div className="col-sm-8">
                                         <select
                                             id="buildingId"
                                             className="form-control border-primary"
@@ -118,11 +119,11 @@ const AddBuildingLocation = () => {
                                         </select>
                                     </div>
                                 </div>
-                                <div className="mb-4 row align-items-center">
+                                <div className="mb-5 row align-items-center">
                                     <label htmlFor="floorId" className="col-sm-4 col-form-label form-label">
                                         <i className="fas fa-map-marker-alt" id="icon-name"></i> Nombre de la ubicación
                                     </label>
-                                    <div className="col-sm-8 w-100 mb-4">
+                                    <div className="col-sm-8">
                                         <input
                                             type="text"
                                             id="floorId"
@@ -134,15 +135,19 @@ const AddBuildingLocation = () => {
                                         />
                                     </div>
                                 </div>
-                                <div className="text-center buttons">
-                                    <Button className="btn btn-danger btn-lg w-100 shadow-sm btn-custom" id='cancel'
-                                            onClick={handleCancel}>
-                                        <i className="fas fa-times"></i> Cancelar
-                                    </Button>
-                                    <Button className="btn btn-lg btn-custom w-100 shadow-sm" type="submit"
-                                            role="button">
-                                        <i className="fas fa-save"></i> Guardar
-                                    </Button>
+                                <div className="mb-4 row align-items-center">
+                                    <div className="col-sm-6">
+                                        <Button className="btn btn-danger btn-lg w-100 shadow-sm btn-custom" id='cancel'
+                                                onClick={handleCancel}>
+                                            <i className="fas fa-times"></i> Cancelar
+                                        </Button>
+                                    </div>
+                                    <div className="col-sm-6">
+                                        <Button className="btn btn-lg btn-custom w-100 shadow-sm" type="submit"
+                                                role="button">
+                                            <i className="fas fa-save"></i> Guardar
+                                        </Button>
+                                    </div>
                                 </div>
                             </form>
 
