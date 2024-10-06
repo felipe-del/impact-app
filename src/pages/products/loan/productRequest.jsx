abb6c257a597211bf0f63916bb948a2b7b3c7fe8
Isaac Felipe Brenes Calderón <isaacfelibrenes1904@gmail.com>
2024-10-06 11:40:02 -0600

    Agregando solicitud de productos

--- src/pages/products/loan/productRequest.jsx
+++ src/pages/products/loan/productRequest.jsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { API_URLS } from '../../../declarations/apiConfig';
 import { usePage } from '../../../context/pageContext';
 
 const ProductRequest = () => {
     const [products, setProducts] = useState([]);
-    const [selectedProductCode, setSelectedProductCode] = useState('');
-    const [quantity, setQuantity] = useState('');
+    const [selectedCategory, setSelectedCategory] = useState('');
     const [purpose, setPurpose] = useState('');
     const [showSuccessAlert, setShowSuccessAlert] = useState(false);
     const [showErrorAlert, setShowErrorAlert] = useState(false);
+    const [filteredCategories, setFilteredCategories] = useState([]); 
+    const [availableQuantity, setAvailableQuantity] = useState(0);
     const formRef = useRef(null);
     const { setPageName } = usePage();
 
@@ -18,60 +18,100 @@ const ProductRequest = () => {
         setPageName("Solicitud de Productos");
     }, [setPageName]);
 
-    const userId = sessionStorage.getItem('userId'); // Replace with your method to get the user ID from session
+    const userId = sessionStorage.getItem('userId'); 
 
     useEffect(() => {
-        // Fetch the available products from the API
-        fetch(`${API_URLS.products}/available`)
-            .then(response => response.json())
-            .then(data => {
-                setProducts(data);
-            })
-            .catch(error => {
-                console.error('Error fetching products:', error);
-                setShowErrorAlert(true);
+        fetch(`http://localhost:8080/product/all`, {
+            method: 'GET',
+            include: 'credentials',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            setProducts(data);
+            console.log(data);
+
+            const categoryCount = {};
+            data.forEach(product => {
+                if (product.status.id === 1) { 
+                    if (!categoryCount[product.category.name]) {
+                        categoryCount[product.category.name] = 0;
+                    }
+                    categoryCount[product.category.name]++;
+                }
             });
+
+            const availableCategories = Object.keys(categoryCount).filter(category => categoryCount[category] > 0);
+            setFilteredCategories(availableCategories);
+        })
+        .catch(error => {
+            console.error('Error fetching products:', error);
+            setShowErrorAlert(true);
+        });
     }, []);
+
+    useEffect(() => {
+        const countAvailableProducts = products.filter(product => 
+            product.category.name === selectedCategory && product.status.id === 1
+        ).length;
+
+        setAvailableQuantity(countAvailableProducts);
+    }, [selectedCategory, products]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        if (!selectedProductCode || !quantity || !purpose) {
+        if (!selectedCategory || !purpose) {
             setShowErrorAlert(true);
             return;
         }
 
+        if (availableQuantity === 0) {
+            setShowErrorAlert(true);
+            return;
+        }
+
+        // Filtrar solo un producto disponible de la categoría seleccionada
+        const selectedProduct = products
+    .filter(product => product.category.name === selectedCategory && product.status.id === 1)
+    .slice(0, 1)[0]?.id; // Obtener el ID del primer producto disponible
+
+
         const productRequest = {
-            productCode: selectedProductCode,
-            quantity,
-            purpose,
-            userId, // Include user ID in the request
-            requestDate: new Date().toISOString().split('T')[0] // Current date
+            categoryName: selectedCategory,  
+            productId: selectedProduct,
+            reason: purpose,               
+            userId: 1,                        
+            requestDate: new Date().toISOString().split('T')[0] 
         };
+        console.log(productRequest);
 
         fetch('http://localhost:8080/product/request', {
             method: 'POST',
+            include: 'credentials',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(productRequest)
         })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                setShowSuccessAlert(true);
-                setSelectedProductCode('');
-                setQuantity('');
-                setPurpose('');
-                if (formRef.current) {
-                    formRef.current.reset();
-                }
-            })
-            .catch(error => {
-                console.error('Error while submitting product request:', error);
-                setShowErrorAlert(true);
-            });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            setShowSuccessAlert(true);
+            setSelectedCategory('');
+            setPurpose('');
+            setAvailableQuantity(0);
+            if (formRef.current) {
+                formRef.current.reset();
+            }
+        })
+        .catch(error => {
+            console.error('Error while submitting product request:', error);
+            setShowErrorAlert(true);
+        });
     };
 
     return (
@@ -80,37 +120,23 @@ const ProductRequest = () => {
                 <h1 id="product-request-title" className="text-center mb-5">Solicitud de Producto</h1>
                 <form ref={formRef} onSubmit={handleSubmit}>
                     <div className="mb-4">
-                        <label htmlFor="productSelect" className="form-label">
-                            <i className="fas fa-box" id="icon-product-code"></i> Seleccione el Producto
+                        <label htmlFor="categorySelect" className="form-label">
+                            <i className="fas fa-box" id="icon-category"></i> Seleccione la Categoría
                         </label>
                         <select
-                            id="productSelect"
+                            id="categorySelect"
                             className="form-select border-primary"
-                            value={selectedProductCode}
-                            onChange={(e) => setSelectedProductCode(e.target.value)}
+                            value={selectedCategory}
+                            onChange={(e) => setSelectedCategory(e.target.value)}
                             required
                         >
-                            <option value="">Seleccione un producto</option>
-                            {products.map(product => (
-                                <option key={product.code} value={product.code}>
-                                    {product.name} - {product.code}
+                            <option value="">Seleccione una categoría</option>
+                            {filteredCategories.map((category) => (
+                                <option key={category} value={category.id}>
+                                    {category}
                                 </option>
                             ))}
                         </select>
-                    </div>
-
-                    <div className="mb-4">
-                        <label htmlFor="quantity" className="form-label">
-                            <i className="fas fa-sort-numeric-up" id="icon-quantity"></i> Cantidad del Producto
-                        </label>
-                        <input
-                            type="number"
-                            id="quantity"
-                            className="form-control border-primary"
-                            value={quantity}
-                            onChange={(e) => setQuantity(e.target.value)}
-                            required
-                        />
                     </div>
 
                     <div className="mb-4">
