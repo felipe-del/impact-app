94e68f6df01712a558c08c83deb9748a95ceded9
JoF00L <131736806+JoF00L@users.noreply.github.com>
2024-10-20 19:08:40 -0600

    feat (apiConfig.js): added all endpoints related to common spaces.

--- src/declarations/apiConfig.js
+++ src/declarations/apiConfig.js
@@ -39,6 +39,27 @@ export const API_URLS = {
         ADD_NEW_LOCATION_TYPE: concat('/asset/locationType'),
         ADD_NEW_LOCATION_NUMBER: concat('/asset/locationNumber')
     },
+    COMMON_SPACE: {
+        ALL_SPACES: concat('/common-space/all'),
+        GET_SPACE_STATUS: concat('/common-space/status'),
+        GET_SPACE_EQUIPMENT: concat('/common-space/equipment'),
+        EQUIPMENT_BY_SPACE: concat('/common-space/equipment-by-space'),
+        GET_BUILDINGS: concat('/common-space/building'),
+        GET_BUILDING_LOCATIONS: concat('/common-space/building-locations'),
+        BUILDING_LOCATIONS_BY_BUILDING: concat('/common-space/locations-by-building'),
+        SPACE_REQUESTS: concat('/common-space/requests'),
+        SPACE_RESERVATIONS: concat('/common-space/reservations'),
+        // CREATE OPERATIONS
+        CREATE_SPACE: concat('/common-space/create'),
+        CREATE_BUILDING: concat('/common-space/create/building'),
+        CREATE_BUILDING_LOCATION: concat('/common-space/create/building-location'),
+        CREATE_EQUIPMENT: concat('/common-space/create/equipment'),
+        // FIND AND EDIT
+        SPACE_BY_ID: (spaceId) => concat(`/common-space/find/space/${spaceId}`),
+        EDIT_SPACE: (spaceId) => concat(`/common-space/edit/space/${spaceId}`),
+        // REQUEST AND RESERVATION
+        REQUEST_AND_RESERVATION: concat('/common-space/request/space-request&reservation')
+    },
     SUPPLIER: {
         GET_ALL: concat('/supplier')
     }
