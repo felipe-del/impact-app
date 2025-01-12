4a45f74908e4dcf883d5c27f629c90258e6bf5f6
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-12 17:03:10 -0600

    Refactor layout and context structure, clean up dependencies.
    Removed unused third-party libraries and redundant imports in `index.html`. Simplified component hierarchy in `App.jsx` and `main.jsx` by eliminating unnecessary context providers and routing complexities. This reduces code bloat and improves maintainability.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import React from 'react';
 import {Outlet} from "react-router-dom";
-import LayoutContext from './context/layoutContext';
-import { PageProvider } from './context/pageContext';
 
 
 const App = () => {
     return (
         <>
-          <PageProvider>
-            <LayoutContext>
-                <Outlet />
-            </LayoutContext>
-          </PageProvider>
+          <Outlet />
         </>
     );
 };
