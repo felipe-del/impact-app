9f1367bea69cc11479d38f867054dcd6714ffac7
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-12 17:03:10 -0600

    Refactor layout and context structure, clean up dependencies.
    Removed unused third-party libraries and redundant imports in `index.html`. Simplified component hierarchy in `App.jsx` and `main.jsx` by eliminating unnecessary context providers and routing complexities. This reduces code bloat and improves maintainability.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
-import { RouterProvider } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
-import './style/index.css'
-import { Routes } from './config/routes'
-import { UserProvider } from './context/userContext'
-import '../src/style/sb-admin-2.css';
+import App from "./App.jsx";
 
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UserProvider>
-      <RouterProvider router={Routes}/>
-    </UserProvider>
+    <App />
   </React.StrictMode>
 )
