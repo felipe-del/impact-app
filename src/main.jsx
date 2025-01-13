045db552d29afb4f9d70ec913fc827e16a74d573
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-13 17:56:03 -0600

    Integrate React Query and routing support.
    Added React Query with QueryClient and Devtools for state management. Replaced `App` with `Router` to enable routing functionality. Included necessary styles to support the updated setup.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Router from "./router.jsx";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import '../src/style/sb-admin-2.css';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+        <Router />
+        <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>,
 )
