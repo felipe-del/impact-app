7c7110bb14b657f98d95e95842b74f7e24b6126c
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-13 17:52:13 -0600

    Add Auth_API for login and user session management
    Introduced a new Auth_API module to handle authentication-related API calls. Includes `login` and `getUser` functions with error handling integration via `handleAxiosError`. This establishes a foundation for managing user authentication.

---
+++ src/api/Auth_API.js
@@ -0,0 +1,22 @@
+import api from "../config/axios.js";
+import handleAxiosError from "./handleAxiosError.js";
+
+const root = '/api/auth'
+
+export async function login(email, password) {
+    try {
+        const { data } = await api.post(`${root}/login`, {email, password})
+        return data
+    } catch (error) {
+        handleAxiosError(error)
+    }
+}
+
+export async function getUser() {
+    try {
+        const { data } = await api.get(`${root}/user-session`)
+        return data
+    } catch (error) {
+        handleAxiosError(error)
+    }
+}
