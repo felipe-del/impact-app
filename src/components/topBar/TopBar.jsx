44d4f9cac592c009358716aaca49461f7624eb01
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-13 17:59:31 -0600

    Add TopBar component with user and logout functionality
    Introduces the TopBar component for consistent navigation, including user information display and a logout modal. This component enhances usability by integrating search, message notifications, and user account actions into the top navigation bar.

---
+++ src/components/topBar/TopBar.jsx
@@ -0,0 +1,97 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import Logout from "../popUp/logout/Logout.jsx";
+
+const TopBar = ({ userName }) => {
+    const [showLogoutModal, setShowLogoutModal] = useState(false);
+    const handleShowLogoutModal = () => setShowLogoutModal(true);
+    const handleCloseLogoutModal = () => setShowLogoutModal(false);
+
+    return (
+        <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+
+            {/* Sidebar Toggle (TopBar) */}
+            <button id="sidebarToggleTop" className="btn btn-link d-md-none rounded-circle mr-3">
+                <i className="fa fa-bars"></i>
+            </button>
+
+            <span className="navbar-text mr-auto d-none d-md-block">
+                Welcome, {userName}!
+            </span>
+            {/* TopBar Navbar */}
+            <ul className="navbar-nav ml-auto">
+                {/* Nav Item - Search Dropdown (Visible Only XS) */}
+                <li className="nav-item dropdown no-arrow d-sm-none">
+                    <a className="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i className="fas fa-search fa-fw"></i>
+                    </a>
+                    {/* Dropdown - Messages */}
+                    <div className="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                        <form className="form-inline mr-auto w-100 navbar-search">
+                            <div className="input-group">
+                                <input type="text" className="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
+                                <div className="input-group-append">
+                                    <button className="btn btn-primary" type="button">
+                                        <i className="fas fa-search fa-sm"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </li>
+
+                {/* Nav Item - Messages */}
+                <li className="nav-item dropdown no-arrow mx-1">
+                    <a className="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i className="fas fa-envelope fa-fw"></i>
+                        <span className="badge badge-danger badge-counter">7</span>
+                    </a>
+                    {/* Dropdown - Messages */}
+                    <div className="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="messagesDropdown">
+                        <h6 className="dropdown-header">Message Center</h6>
+                        {/* Message items here... */}
+                        <a className="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
+                    </div>
+                </li>
+
+                <div className="topbar-divider d-none d-sm-block"></div>
+
+                {/* Nav Item - User Information */}
+                <li className="nav-item dropdown no-arrow">
+                    <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <img className="img-profile rounded-circle" src='/undraw_profile.svg' alt="Profile" />
+                    </a>
+                    {/* Dropdown - User Information */}
+                    <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                        <a className="dropdown-item" href="#">
+                            <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Profile
+                        </a>
+                        <a className="dropdown-item" href="#">
+                            <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Settings
+                        </a>
+                        <a className="dropdown-item" href="#">
+                            <i className="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Activity Log
+                        </a>
+                        <div className="dropdown-divider"></div>
+                        <a className="dropdown-item" href="#" onClick={handleShowLogoutModal}>
+                            <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Logout
+                        </a>
+                    </div>
+                </li>
+            </ul>
+
+            <Logout show={showLogoutModal} onClose={handleCloseLogoutModal} onHide={handleCloseLogoutModal}/>
+        </nav>
+    );
+};
+
+TopBar.propTypes = {
+    userName: PropTypes.string,
+};
+
+export default TopBar;
