87c49a3ca0bc82088668ae397d45140ae5c8ec52
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-13 17:51:55 -0600

    Remove unused App.jsx file
    The App.jsx file was deleted as it is no longer needed in the codebase. This helps to keep the project clean and maintainable by removing dead code.

---
+++ src/layouts/AppLayout.jsx
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { Navigate } from "react-router-dom";
+import {getUser} from "../api/Auth_API.js";
+import IMPACT from "../components/IMPACT.jsx";
+
+export default function AppLayout() {
+
+    const { data, isLoading, isError } = useQuery({
+        queryFn: getUser,
+        queryKey: ['user'],
+        retry: 2,
+        refetchOnWindowFocus: false
+    })
+    if(isLoading) return <p>Cargando...</p>
+    if(isError) return <Navigate to={'/auth'} />
+    if (data) return <IMPACT responseWrapper={data}/>
+}
