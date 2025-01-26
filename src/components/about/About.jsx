ea3cd8a1422d5bfe0e7d06afc2f76e92d4c9c2f8
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-25 22:17:35 -0600

    Style about page with improved layout and modern design.
    Updated the about page styles to enhance readability, layout, and visual appeal. Introduced new classes, including content centering, animations, and hover effects. Replaced the generic button and container styles with more specific and consistent naming conventions.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,20 +3,15 @@ import './about.css';
 
 const About = () => {
     return (
-        <div className="about-container">
-            <h1>Sobre Nosotros</h1>
-            <p>
-                ¡Bienvenido a la página de Sobre Nosotros de nuestra aplicación! Nos dedicamos a ofrecer el mejor servicio y experiencia de usuario.
-                Nuestro equipo está apasionado por construir soluciones de software de alta calidad que cumplan con las necesidades de nuestros clientes y usuarios.
-            </p>
-            <p>
-                Nuestra misión es ofrecer productos innovadores y confiables que ayuden a agilizar y mejorar tus tareas diarias.
-                Nos esforzamos continuamente por mejorar nuestros servicios y adaptarnos a las demandas cambiantes del mercado.
-            </p>
-            <p>
-                Gracias por visitar nuestra página de Sobre Nosotros. Si tienes alguna pregunta o necesitas más información, no dudes en ponerte en contacto con nosotros.
-            </p>
-            <Link to="/auth" className="login-button">Comenzar!</Link>
+        <div className="about-page-container">
+            <div className="about-page-content">
+                <img src="/IMPACT_BLACK_LOGO.png" alt="IMPACT LOGO" className="about-page-logo" />
+                <h1 className="about-page-heading">¡Bienvenido!</h1>
+                <p className="about-page-description">
+                    Gracias por visitar nuestra página. Si tienes alguna pregunta o necesitas más información, no dudes en ponerte en contacto con nosotros.
+                </p>
+                <Link to="/auth" className="about-page-button">Comenzar!</Link>
+            </div>
         </div>
     );
 };
