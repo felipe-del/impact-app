c796e4c1787003631b5bcbd459f31a0f97e5f07b
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-26 15:04:15 -0600

    Add NotFound component for 404 error handling
    Introduce a new NotFound component with a styled UI to handle nonexistent routes. Includes a logo, message, and link to navigate back to the homepage. This ensures a better user experience for undefined paths.

---
+++ src/components/notFound/NotFound.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import './notFound.css';
+
+const NotFound = () => {
+    return (
+        <div className="notfound-page-container">
+            <div className="notfound-page-content">
+                <img src="/IMPACT_BLACK_LOGO.png" alt="IMPACT LOGO" className="notfound-page-logo" />
+                <h1 className="notfound-page-heading">¡Oops! Página No Encontrada</h1>
+                <p className="notfound-page-description">
+                    Lo sentimos, pero la página que estás buscando no existe. Es posible que hayas ingresado una URL incorrecta o que la página haya sido movida.
+                </p>
+                <Link to="/" className="notfound-page-button">Regresar al Inicio</Link>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
