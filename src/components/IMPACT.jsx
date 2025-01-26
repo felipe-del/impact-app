73869351dae23cd3bb795782ddc35e263ce4e59b
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-25 22:19:37 -0600

    Refactor IMPACT component to support children prop.
    Updated the IMPACT component to accept and render a `children` prop, allowing greater flexibility for custom content. Also, streamlined role extraction logic and added prop type validation for the new `children` prop.

--- src/components/IMPACT.jsx
+++ src/components/IMPACT.jsx
@@ -2,19 +2,28 @@ import PropTypes from 'prop-types';
 import Sidebar from "./sidebar/Sidebar.jsx";
 import TopBar from "./topBar/TopBar.jsx";
 
-export default function IMPACT({ responseWrapper }) {
+export default function IMPACT({ responseWrapper, children }) {
     const user = responseWrapper.data;
-    console.log(user);
+    const role = user.userRoleResponse.roleName;
     return (
         <div id="wrapper">
             {/* Sidebar */}
-            <Sidebar role={user.userRoleResponse.roleName} />
+            <Sidebar role={role} />
             {/* Content Wrapper */}
             <div id="content-wrapper" className="d-flex flex-column">
                 {/* Main Content */}
                 <div id="content">
                     {/* TopBar */}
                     <TopBar userName={user.name}/>
+
+                    {/* Page Content */}
+                    <div className="container-fluid">
+                        <h1 className="h3 mb-4 text-gray-800">{
+                            children || (<h1 className="h3 mb-4 text-gray-800">IMPACT</h1>
+                            )}
+                        </h1>
+                    </div>
+
                 </div>
             </div>
         </div>
@@ -22,4 +31,5 @@ export default function IMPACT({ responseWrapper }) {
 }
 IMPACT.propTypes = {
     responseWrapper: PropTypes.any.isRequired,
+    children: PropTypes.node
 };
