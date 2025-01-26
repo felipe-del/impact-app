7446434920e1b65ddd01cce16b2c0ea515b021cc
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-26 15:02:50 -0600

    Switch to react-hot-toast and adjust Toaster settings
    Replaced 'sonner' with 'react-hot-toast' for toast notifications. Updated Toaster's position to 'bottom-right' and added 'reverseOrder' prop for improved flexibility.

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom"
-import { Toaster} from 'sonner'
+import { Toaster} from 'react-hot-toast'
 
 export default function AuthLayout() {
     return(
         <>
             <Outlet/>
-            <Toaster position="top-right"/>
+            <Toaster
+                position="bottom-right"
+                reverseOrder={false}
+            />
         </>
     )
 }
