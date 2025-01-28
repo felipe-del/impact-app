b57024d842a60b3730d416aeaa076a90a3c2f8b8
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-27 18:19:28 -0600

    Add ResetPasswordToggle component for password recovery
    Introduces a new component to handle password reset workflows. It supports toggling between sending a reset email and entering a token to create a new password. Includes navigation handling and token-based authentication redirect.Add ResetPasswordToggle component and update routing
    
    Introduce the ResetPasswordToggle for handling password reset functionality and integrate it into the routing system. Adjust existing paths and add new routes for better organization and accessibility, including user management and password reset.

--- src/router.jsx
+++ src/router.jsx
@@ -1,23 +1,29 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import AuthToggle from "./views/auth/AuthToggle.jsx";
+import AuthToggle from "./views/auth/loginAndRegister/AuthToggle.jsx";
 import AuthLayout from "./layouts/AuthLayout.jsx";
 import AppLayout from "./layouts/AppLayout.jsx";
 import About from "./components/about/About.jsx";
 import DataTable from "./components/DataTable.jsx";
 import NotFound from "./components/notFound/NotFound.jsx";
+import ResetPasswordToggle from "./views/auth/changePassword/ResetPasswordToggle.jsx";
+import UserManagement from "./views/user/UserManagement.jsx";
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
+
                 <Route path={'/'} element={<About/>}/>
 
                 <Route element={<AuthLayout/>}>
                     <Route path='/auth' element={<AuthToggle/>}/>
                 </Route>
 
-                <Route element={<AppLayout />}>
-                    <Route path="/app" element={<DataTable />} />
+                <Route path='/forgot-password' element={<ResetPasswordToggle/>}/>
+
+                <Route element={<AppLayout /> } path="/app">
+                    <Route index element={<DataTable />} />
+                    <Route path="userManagement" element={<UserManagement/>}/>
                 </Route>
 
                 {/* Page not found */}
