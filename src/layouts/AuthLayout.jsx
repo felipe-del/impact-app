24e00cf14826cbc38a2ef21c434b45492e831bf0
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-27 18:17:09 -0600

    Remove Toaster component from AuthLayout
    The Toaster component was removed as it is unnecessary in the AuthLayout. Simplifies the layout by focusing solely on rendering the child route components.

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,14 +1,9 @@
 import { Outlet } from "react-router-dom"
-import { Toaster} from 'react-hot-toast'
 
 export default function AuthLayout() {
     return(
         <>
             <Outlet/>
-            <Toaster
-                position="bottom-right"
-                reverseOrder={false}
-            />
         </>
     )
 }
