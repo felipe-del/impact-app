81c6fc76204ce2a70ca4ee21ceaa94fb4085282a
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-27 18:20:28 -0600

    Add User API module with getAllUsers function
    Introduces a new User_API.js module to interact with the user API. Implements the `getAllUsers` function to fetch all user data and handles potential errors using the existing `handleAxiosError` utility. This establishes the groundwork for user-related API calls.

---
+++ src/api/User_API.js
@@ -0,0 +1,13 @@
+import api from "../config/axios.js";
+import handleAxiosError from "./handleAxiosError.js";
+
+const root = '/api/user'
+
+export async function getAllUsers() {
+    try {
+        const { data } = await api.get(root)
+        return data
+    } catch (error) {
+        handleAxiosError(error)
+    }
+}
