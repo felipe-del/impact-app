92f8b0209c6563e152f686e09c57e401639afa82
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-27 18:18:25 -0600

    Refactor error handling to improve message clarity
    Replaced inline error message extraction with a dedicated `createErrorMessage` function. This enhances readability and ensures consistent formatting of backend error messages.

--- src/api/handleAxiosError.js
+++ src/api/handleAxiosError.js
@@ -2,9 +2,13 @@ import {isAxiosError} from "axios";
 
 export default function handleAxiosError(error) {
     if (isAxiosError(error) && error.response) {
-        const backendError = error.response.data?.message || 'Error de conexión'
+        const backendError = createErrorMessage(error.response.data)
         throw new Error(backendError)
     } else {
         throw new Error('Error de conexión')
     }
+}
+
+function createErrorMessage(responseWrapper) {
+    return `${responseWrapper.message} - ${responseWrapper.data.message}`
 }
