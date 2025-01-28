06388c552308b170f1a9669fec2d68dd4fe447f3
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-27 18:18:33 -0600

    Add toast notifications with react-hot-toast
    Integrated the Toaster component for displaying toast notifications, positioned at the bottom-right of the screen. Commented out ReactQueryDevtools for now to streamline the UI during this update.

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Router from "./router.jsx";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+//import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import '../src/style/sb-admin-2.css';
+import {Toaster} from "react-hot-toast";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+        <Toaster position="bottom-right" reverseOrder={false}/> {/* Toast notifications */}
         <Router />
         {/* <ReactQueryDevtools /> */}
     </QueryClientProvider>
