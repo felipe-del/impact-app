f9d9b8365d9be60d56917176908a422cd51f803a
felipe-del <isaacfelibrenes1904@gmail.com>
2025-01-30 22:20:18 -0600

    Refactor routes and simplify layout structure
    Reorganized component imports and updated paths for consistency. Removed nested `AuthLayout` and streamlined routing for `AuthToggle`. Adjusted file structure to better align with the views directory convention.

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,9 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import AuthToggle from "./views/auth/loginAndRegister/AuthToggle.jsx";
-import AuthLayout from "./layouts/AuthLayout.jsx";
+
 import AppLayout from "./layouts/AppLayout.jsx";
-import About from "./components/about/About.jsx";
-import DataTable from "./components/DataTable.jsx";
-import NotFound from "./components/notFound/NotFound.jsx";
+import About from "./views/about/About.jsx";
+import NotFound from "./views/notFound/NotFound.jsx";
 import ResetPasswordToggle from "./views/auth/changePassword/ResetPasswordToggle.jsx";
 import UserManagement from "./views/user/UserManagement.jsx";
 
@@ -15,19 +14,18 @@ export default function Router() {
 
                 <Route path={'/'} element={<About/>}/>
 
-                <Route element={<AuthLayout/>}>
-                    <Route path='/auth' element={<AuthToggle/>}/>
-                </Route>
+                <Route path='/auth' element={<AuthToggle/>}/>
 
                 <Route path='/forgot-password' element={<ResetPasswordToggle/>}/>
 
                 <Route element={<AppLayout /> } path="/app">
-                    <Route index element={<DataTable />} />
                     <Route path="userManagement" element={<UserManagement/>}/>
                 </Route>
 
                 {/* Page not found */}
                 <Route path="*" element={<NotFound />} />
+
+
 
             </Routes>
         </BrowserRouter>
