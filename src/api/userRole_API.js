c88a542962a1e556e6f5ee81286875a7306413b2
felipe-del <isaacfelibrenes1904@gmail.com>
2025-02-02 11:59:51 -0600

    Add user role API function to fetch all user roles
    Introduce `getAllUserRoles` function in `userRole_API.js` to retrieve user role data from the server. Includes error handling using `handleAxiosError`. This is part of the `/api/user-role` endpoint integration.

---
+++ src/api/userRole_API.js
@@ -0,0 +1,13 @@
+import api from "../config/axios.js";
+import handleAxiosError from "./handleAxiosError.js";
+
+const root = '/api/user-role'
+
+export async function getAllUserRoles() {
+    try {
+        const { data } = await api.get(root)
+        return data
+    } catch (error) {
+        handleAxiosError(error)
+    }
+}
