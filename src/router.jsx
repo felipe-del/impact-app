a73fc8727f7e053f1dd428bd80576d1daa8a340c
felipe-del <isaacfelibrenes1904@gmail.com>
2025-02-01 23:57:24 -0600

    Rename UserManagement to UserTable in router configuration
    Updated the route and import for user management component to reflect the new naming convention, improving clarity and consistency. No functional changes were made to the behavior of the route.

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,10 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import AuthToggle from "./views/auth/loginAndRegister/AuthToggle.jsx";
-
 import AppLayout from "./layouts/AppLayout.jsx";
 import About from "./views/about/About.jsx";
 import NotFound from "./views/notFound/NotFound.jsx";
 import ResetPasswordToggle from "./views/auth/changePassword/ResetPasswordToggle.jsx";
-import UserManagement from "./views/user/UserManagement.jsx";
+import UserTable from "./views/user/UserTable.jsx";
 
 export default function Router() {
     return (
@@ -19,7 +18,7 @@ export default function Router() {
                 <Route path='/forgot-password' element={<ResetPasswordToggle/>}/>
 
                 <Route element={<AppLayout /> } path="/app">
-                    <Route path="userManagement" element={<UserManagement/>}/>
+                    <Route path="userTable" element={<UserTable/>}/>
                 </Route>
 
                 {/* Page not found */}
