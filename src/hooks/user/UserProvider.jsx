ac2d267f03533255fb98bd5f3d002196d4e2d097
felipe-del <isaacfelibrenes1904@gmail.com>
2025-02-08 22:27:33 -0600

    Add UserProvider component with context for user data
    Introduce `UserProvider` using React's Context API to manage and share user data across the application. Includes PropTypes validation for better reliability and clarity. This sets up a foundation for consistent user state management.

---
+++ src/hooks/user/UserProvider.jsx
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+import PropTypes from "prop-types";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children, user }) => {
+    return (
+        <UserContext.Provider value={user}>
+            {children}
+        </UserContext.Provider>
+    );
+};
+
+UserProvider.propTypes = {
+    children: PropTypes.node.isRequired,
+    user: PropTypes.any.isRequired,
+};
