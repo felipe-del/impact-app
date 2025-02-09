961b393d9be08e902f09a5adce2d13821287df55
felipe-del <isaacfelibrenes1904@gmail.com>
2025-02-08 22:24:48 -0600

    Refactor IMPACT component to use centralized UserProvider.
    Replaces locally defined UserProvider with an imported one from hooks, simplifying the codebase and promoting reusability. Minor code style adjustments were also made for consistency.

--- src/components/IMPACT.jsx
+++ src/components/IMPACT.jsx
@@ -1,28 +1,8 @@
-import { createContext, useContext, useState } from 'react';
-
+import { useState } from "react";
 import PropTypes from "prop-types";
 import Sidebar from "./sidebar/Sidebar.jsx";
 import TopBar from "./topBar/TopBar.jsx";
-
-
-const UserContext = createContext();
-
-export const UserProvider = ({ children, user }) => {
-UserProvider.propTypes = {
-    children: PropTypes.node.isRequired,
-    user: PropTypes.any.isRequired,
-};
-    return (
-        <UserContext.Provider value={user}>
-            {children}
-        </UserContext.Provider>
-    );
-};
-
-// Hook para consumir el contexto
-export const useUser = () => {
-    return useContext(UserContext);
-};
+import {UserProvider} from "../hooks/user/UserProvider.jsx";
 
 export default function IMPACT({ responseWrapper, children }) {
     const [user] = useState(responseWrapper.data);  // Suponemos que el `responseWrapper.data` es el usuario
@@ -31,13 +11,13 @@ export default function IMPACT({ responseWrapper, children }) {
         <UserProvider user={user}>
             <div id="wrapper">
                 {/* Sidebar */}
-                <Sidebar role={user?.userRoleResponse?.roleName || 'Default Role'} />
+                <Sidebar role={user?.userRoleResponse?.roleName || "Default Role"} />
                 {/* Content Wrapper */}
                 <div id="content-wrapper" className="d-flex flex-column">
                     {/* Main Content */}
                     <div id="content">
                         {/* TopBar */}
-                        <TopBar user={user || { userName: 'Default User' }} />
+                        <TopBar user={user || { userName: "Default User" }} />
                         {/* Page Content */}
                         <div className="container-fluid">
                             <h1 className="h3 mb-4 text-gray-800">
@@ -53,5 +33,5 @@ export default function IMPACT({ responseWrapper, children }) {
 
 IMPACT.propTypes = {
     responseWrapper: PropTypes.any.isRequired,
-    children: PropTypes.node
+    children: PropTypes.node,
 };
