2c36856bba29f737bbbeb8d04aa9de4e5ba3f723
felipe-del <isaacfelibrenes1904@gmail.com>
2025-02-08 22:25:28 -0600

    Rename NotFound folder and update "Regresar" button behavior
    The NotFound component folder was moved from "notFound" to "exception/notFound" to better reflect its purpose. Additionally, the "Regresar al Inicio" button was modified to use `window.history.back()` instead of linking to the home page, improving user navigation.

---
+++ src/config/protectedRoute.jsx
@@ -0,0 +1,27 @@
+import {Navigate, Outlet, useNavigate} from "react-router-dom";
+import PropTypes from 'prop-types';
+import {useUser} from "../hooks/user/useUser.jsx";
+
+
+const ProtectedRoute = ({ allowedRoles }) => {
+
+    const user = useUser();
+
+    const navigate = useNavigate();
+
+    if (!user) {
+        navigate('/auth/login');
+    }
+
+    if (!allowedRoles.includes(user.userRoleResponse.roleName)) {
+        return <Navigate to="/unauthorized" replace />;
+    }
+
+    return <Outlet />;
+};
+
+ProtectedRoute.propTypes = {
+    allowedRoles: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default ProtectedRoute;
