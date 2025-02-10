3ac6f77bff482eae5f9beceb1bda02fce411a54e
felipe-del <isaacfelibrenes1904@gmail.com>
2025-02-10 16:17:49 -0600

    Add location number API module with CRUD operations
    Introduce a new API module for managing location numbers, including functions to create, read, update, and delete location number records. Error handling is integrated using `handleAxiosError` to manage exceptions effectively.

---
+++ src/api/locationNumber_API/locationNumber_API.js
@@ -0,0 +1,40 @@
+import api from "../../config/axios.js";
+import handleAxiosError from "../handleAxiosError.js";
+
+const root = '/api/location-number'
+
+export async function updateLocationNumber(id, locationNumber) {
+    try {
+        const { data } = await api.get(`${root}/${id}`, locationNumber)
+        return data
+    } catch (error) {
+        handleAxiosError(error)
+    }
+}
+
+export async function deleteLocationNumber(id) {
+    try {
+        const response = await api.delete(`${root}/${id}`)
+        return response.data
+    } catch (error) {
+        handleAxiosError(error)
+    }
+}
+
+export async function getAllLocationNumber() {
+    try {
+        const { data } = await api.get(root)
+        return data
+    } catch (error) {
+        handleAxiosError(error)
+    }
+}
+
+export async function saveLocationNumber(locationNumber) {
+    try {
+        const { data } = await api.post(root, locationNumber)
+        return data
+    } catch (error) {
+        handleAxiosError(error)
+    }
+}
