5595bf255b1df8a05b815dee901b5800b0827697
felipe-del <isaacfelibrenes1904@gmail.com>
2025-02-10 16:14:08 -0600

    Update user query function to use getUserSession
    Replaced getUser with getUserSession in AppLayout to align with updated API changes. This ensures proper user session management and consistency with recent backend updates.

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import {Navigate, Outlet} from "react-router-dom";
-import {getUser} from "../api/auth_API.js";
+import {getUserSession} from "../api/auth_API.js";
 import IMPACT from "../components/IMPACT.jsx";
 
 import LoadingSpinner from "../components/spinner/loadingSpinner/LoadingSpinner.jsx";
@@ -8,7 +8,7 @@ import LoadingSpinner from "../components/spinner/loadingSpinner/LoadingSpinner.
 export default function AppLayout() {
 
     const { data, isLoading, isError } = useQuery({
-        queryFn: getUser,
+        queryFn: getUserSession,
         queryKey: ['user'],
         retry: 2,
         refetchOnWindowFocus: false
