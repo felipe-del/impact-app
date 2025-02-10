f1d83904bf9c438748e60e1459a5afd068194d3f
felipe-del <isaacfelibrenes1904@gmail.com>
2025-02-10 16:17:35 -0600

    Add brand API module with CRUD operations
    Introduced a new brand_API.js file to manage brand-related operations. Includes methods for creating, reading, updating, and deleting brand data, utilizing Axios for HTTP requests and centralized error handling. This addition prepares the API layer for brand management features.

---
+++ src/api/brand/brand_API.js
@@ -0,0 +1,41 @@
+
+import handleAxiosError from "../handleAxiosError.js";
+import api from "../../config/axios.js";
+
+const root = '/api/brand'
+
+export async function updateBrand(id, brand) {
+    try {
+        const { data } = await api.get(`${root}/${id}`, brand)
+        return data
+    } catch (error) {
+        handleAxiosError(error)
+    }
+}
+
+export async function deleteBrand(id) {
+    try {
+        const response = await api.delete(`${root}/${id}`)
+        return response.data
+    } catch (error) {
+        handleAxiosError(error)
+    }
+}
+
+export async function getAllBrands() {
+    try {
+        const { data } = await api.get(root)
+        return data
+    } catch (error) {
+        handleAxiosError(error)
+    }
+}
+
+export async function saveBrand(brand) {
+    try {
+        const { data } = await api.post(root, brand)
+        return data
+    } catch (error) {
+        handleAxiosError(error)
+    }
+}
