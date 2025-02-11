ff5e931a731269e846c9a634a5f25547a8fd79be
felipe-del <isaacfelibrenes1904@gmail.com>
2025-02-11 12:32:59 -0600

    Enhance useUser hook with null check and error handling
    Add a null check to the useUser hook to ensure it is used within a UserProvider. This change improves error handling by throwing a clear error message if the hook is misused.

--- src/hooks/user/useUser.jsx
+++ src/hooks/user/useUser.jsx
@@ -2,5 +2,9 @@ import { useContext } from "react";
 import { UserContext } from "./UserProvider";
 
 export const useUser = () => {
-    return useContext(UserContext);
+    const user = useContext(UserContext);
+    if (user === null) {
+        throw new Error("useUser debe ser usado dentro de un UserProvider");
+    }
+    return user;
 };
