438ad489e22a9cdd7c5d0093900598165a6a03bd
felipe-del <isaacfelibrenes1904@gmail.com>
2025-02-13 13:45:47 -0600

    Refactor user API file structure for better organization.
    Moved user-related API files to a dedicated `user` directory. Updated import paths accordingly to reflect the new structure. This change improves maintainability and categorization of user-related APIs.

--- src/api/user/user_API.js
+++ src/api/user/user_API.js
@@ -1,5 +1,5 @@
-import api from "../config/axios.js";
-import handleAxiosError from "./handleAxiosError.js";
+import api from "../../config/axios.js";
+import handleAxiosError from "../handleAxiosError.js";
 
 const root = '/api/user'
 
