749d2a3ab2b580b166a0024a5a440efe918d7353
felipe-del <isaacfelibrenes1904@gmail.com>
2025-02-12 22:08:47 -0600

    Add `codeStyle.js` for banner and button styling
    This commit introduces a `codeStyle.js` file to centralize styles for the banner and buttons. It includes flexbox layouts, gradients, and responsive design elements to enhance consistency and maintainability across components.

---
+++ src/style/codeStyle.js
@@ -0,0 +1,33 @@
+export const bannerStyle = {
+    banner: {
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "15px 15px",
+        marginBottom: "20px",
+        backgroundColor: "white",
+        boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+        position: "sticky",
+        borderRadius: "15px",
+        top: 0,
+        zIndex: 100,
+    },
+    title: {
+        fontFamily: "Montserrat, sans-serif",
+        fontSize: "20px",
+        margin: 5,
+        background: "linear-gradient(45deg, #005DA4, #0078D4)",
+        WebkitBackgroundClip: "text",
+        color: "transparent",
+    },
+    buttonsContainer: {
+        display: "flex",
+        justifyContent: "flex-end",
+        gap: "15px",
+    },
+    button: {
+        fontFamily: "Montserrat, sans-serif",
+        borderRadius: "15px",
+        textTransform: "none",
+        fontSize: "12px",
+    },
+};
