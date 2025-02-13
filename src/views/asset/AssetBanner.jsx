98f61c0ec44268b137ba4a91669c84c62d918b45
felipe-del <isaacfelibrenes1904@gmail.com>
2025-02-12 22:08:05 -0600

    Refactor AssetBanner to use BannerActionButton and centralized styles.
    Replaced ActionButton with BannerActionButton to align with naming conventions and improved styling. Centralized styles by moving them to a separate `bannerStyle` module to enhance maintainability and reusability across components. Simplified the structure of the banner component for better readability.

--- src/views/asset/AssetBanner.jsx
+++ src/views/asset/AssetBanner.jsx
@@ -5,7 +5,8 @@ import ManageHistoryIcon from '@mui/icons-material/ManageHistory';
 import { CSVLink } from "react-csv";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
-import ActionButton from "../../components/button/ActionButton.jsx";
+import BannerActionButton from "../../components/button/BannerActionButton.jsx";
+import {bannerStyle} from "../../style/codeStyle.js";
 
 const AssetBanner = ({ title = "",
                          visibleButtons = ["csv", "pdf", "statusModal", "export", "createAsset"],
@@ -25,13 +26,24 @@ const AssetBanner = ({ title = "",
         setAnchorEl(null);
     };
 
+    const styles = bannerStyle;
+
     return (
         <div className="export-buttons" style={styles.banner}>
             <h3 style={styles.title}>{title}</h3>
             <div style={styles.buttonsContainer}>
+                {visibleButtons.includes("goBack") && (
+                    <BannerActionButton
+                        onClick={() => navigate(-1)}
+                        text={"Volver"}
+                        icon={<ArrowBack />}
+                        color={"error"}
+                        style={styles.button}
+                    />
 
+                )}
                 {visibleButtons.includes("createAsset") && (
-                    <ActionButton
+                    <BannerActionButton
                         onClick={() => navigate("/app/createAsset")}
                         text={"Crear Activo"} icon={<PersonAdd />}
                         color="error"
@@ -40,7 +52,7 @@ const AssetBanner = ({ title = "",
                     />
                 )}
                 {visibleButtons.includes("statusModal") && (
-                    <ActionButton
+                    <BannerActionButton
                         onClick={handleOpen}
                         text={"Estados del Activo"}
                         icon={<ManageHistoryIcon />}
@@ -50,18 +62,8 @@ const AssetBanner = ({ title = "",
                     />
 
                 )}
-                {visibleButtons.includes("goBack") && (
-                    <ActionButton
-                        onClick={() => navigate(-1)}
-                        text={"Volver"}
-                        icon={<ArrowBack />}
-                        color={"error"}
-                        style={styles.button}
-                    />
-
-                )}
                 {visibleButtons.includes("export") && (
-                    <ActionButton
+                    <BannerActionButton
                         onClick={handleClick}
                         icon={<FileDownload />}
                         text="Exportar"
@@ -105,36 +107,5 @@ AssetBanner.propTypes = {
     visibleButtons: PropTypes.arrayOf(PropTypes.string),
 };
 
-const styles = {
-    banner: {
-        display: "flex",
-        justifyContent: "space-between",
-        padding: "15px 15px",
-        marginBottom: "20px",
-        backgroundColor: "white",
-        boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-        position: "sticky",
-        borderRadius: "15px",
-        top: 0,
-        zIndex: 100,
-    },
-    title: {
-        fontFamily: "Montserrat, sans-serif",
-        fontSize: "20px",
-        margin: 5,
-        color: "#333",
-    },
-    buttonsContainer: {
-        display: "flex",
-        justifyContent: "flex-end",
-        gap: "15px",
-    },
-    button: {
-        fontFamily: "Montserrat, sans-serif",
-        borderRadius: "15px",
-        textTransform: "none",
-        fontSize: "12px",
-    },
-};
 
 export default AssetBanner;
