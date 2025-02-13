fce965bee2262e7e17bf92753a2e409fe8f2486a
felipe-del <isaacfelibrenes1904@gmail.com>
2025-02-13 13:42:16 -0600

    Refactor About.jsx layout with updated container element
    Replaced the outermost `<div>` with a semantic `<main>` element and added a "background" class for styling. This improves semantic structure and potential future maintainability of the layout.

--- src/views/about/About.jsx
+++ src/views/about/About.jsx
@@ -3,16 +3,18 @@ import './about.css';
 
 const About = () => {
     return (
-        <div className="about-page-container">
-            <div className="about-page-content">
-                <img src="/IMPACT_BLACK_LOGO.png" alt="IMPACT LOGO" className="about-page-logo" />
-                <h1 className="about-page-heading">¡Bienvenido!</h1>
-                <p className="about-page-description">
-                    Gracias por visitar nuestra página. Si tienes alguna pregunta o necesitas más información, no dudes en ponerte en contacto con nosotros.
-                </p>
-                <Link to="/auth" className="about-page-button">Comenzar!</Link>
+        <main className={"background"}>
+            <div className="about-page-container">
+                <div className="about-page-content">
+                    <img src="/IMPACT_BLACK_LOGO.png" alt="IMPACT LOGO" className="about-page-logo" />
+                    <h1 className="about-page-heading">¡Bienvenido!</h1>
+                    <p className="about-page-description">
+                        Gracias por visitar nuestra página. Si tienes alguna pregunta o necesitas más información, no dudes en ponerte en contacto con nosotros.
+                    </p>
+                    <Link to="/auth" className="about-page-button">Comenzar!</Link>
+                </div>
             </div>
-        </div>
+        </main>
     );
 };
 
