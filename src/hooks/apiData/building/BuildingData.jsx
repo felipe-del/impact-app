0892edadc5791efeb62c174dece2df65d4fb1e60
felipe-del <isaacfelibrenes1904@gmail.com>
2025-02-23 19:22:41 -0600

    Refactor BrandManagement and enhance loading UX
    Removed unused useCreateBrand function and replaced plain loading text with LoadingPointsSpinner for improved user feedback during data loading. Simplifies code for brand creation and streamlines user interface.

---
+++ src/hooks/apiData/building/BuildingData.jsx
@@ -0,0 +1,20 @@
+import {useQuery} from "@tanstack/react-query";
+import {getAllBuilding} from "../../../api/buildingLocation/buildingLocation_API.js";
+
+
+const useBuildingData = () => {
+    const { data: wrapperResponse, isLoading, isError, refetch} = useQuery({
+        queryKey: ['building'],
+        queryFn: getAllBuilding,
+    });
+
+    return {
+        building: wrapperResponse || [],
+        isLoading,
+        isError,
+        refetch
+    };
+};
+
+
+export default useBuildingData;
