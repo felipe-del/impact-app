21d42babaa95520e641281143b8d5d3319c8dc3d
felipe-del <isaacfelibrenes1904@gmail.com>
2025-02-23 19:27:17 -0600

    Add SpaceBanner component for space-related actions
    Introduce the SpaceBanner component to display a title and action buttons, such as "Go Back" and "Add Space," based on provided props. This enhances reusability and modularity for space management views.

---
+++ src/views/space/SpaceBanner.jsx
@@ -0,0 +1,48 @@
+import { ArrowBack, AddBusiness } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
+import BannerActionButton from "../../components/button/BannerActionButton.jsx";
+import {bannerStyle} from "../../style/codeStyle.js";
+
+const SpaceBanner = ({ title = "", visibleButtons = [""], createSpace}) => {
+    const navigate = useNavigate();
+
+    const styles = bannerStyle;
+
+    return (
+        <div className="export-buttons" style={styles.banner}>
+            <h3 style={styles.title}>{title}</h3>
+            <div style={styles.buttonsContainer}>
+                {visibleButtons.includes("goBack") && (
+                    <BannerActionButton
+                        onClick={() => navigate(-1)}
+                        text={"Volver"}
+                        icon={<ArrowBack />}
+                        color={"error"}
+                        style={styles.button}
+                    />
+
+                )}
+                {visibleButtons.includes("createSpace") && (
+                    <BannerActionButton
+                        onClick={createSpace}
+                        text={"Agregar Espacio"} icon={<AddBusiness  />}
+                        color="error"
+                        style={styles.button}
+                        widthWhenIsHover={"170px"}
+                    />
+                )}
+
+            </div>
+        </div>
+    );
+};
+
+SpaceBanner.propTypes = {
+    title: PropTypes.string,
+    visibleButtons: PropTypes.arrayOf(PropTypes.string),
+    createSpace: PropTypes.func,
+};
+
+
+export default SpaceBanner;
