0d2bc8210509f055b001392ff0f7f8cf9de0355a
felipe-del <isaacfelibrenes1904@gmail.com>
2025-02-23 19:25:22 -0600

    Update NotFound page layout and add background styling
    Wrapped content in a new <main> tag with a CSS class for a background. Added styling to include a background image with proper positioning and scaling for the NotFound page. This enhances the page's visual appeal.

--- src/views/exception/notFound/NotFound.jsx
+++ src/views/exception/notFound/NotFound.jsx
@@ -3,16 +3,18 @@ import './notFound.css';
 
 const NotFound = () => {
     return (
-        <div className="notfound-page-container">
-            <div className="notfound-page-content">
-                <img src="/IMPACT_BLACK_LOGO.png" alt="IMPACT LOGO" className="notfound-page-logo" />
-                <h1 className="notfound-page-heading">¡Oops! Página No Encontrada</h1>
-                <p className="notfound-page-description">
-                    Lo sentimos, pero la página que estás buscando no existe. Es posible que hayas ingresado una URL incorrecta o que la página haya sido movida.
-                </p>
-                <Link to="#" onClick={() => window.history.back()} className="notfound-page-button">Regresar</Link>
+        <main className={"background-notfound"}>
+            <div className="notfound-page-container">
+                <div className="notfound-page-content">
+                    <img src="/IMPACT_BLACK_LOGO.png" alt="IMPACT LOGO" className="notfound-page-logo" />
+                    <h1 className="notfound-page-heading">¡Oops! Página No Encontrada</h1>
+                    <p className="notfound-page-description">
+                        Lo sentimos, pero la página que estás buscando no existe. Es posible que hayas ingresado una URL incorrecta o que la página haya sido movida.
+                    </p>
+                    <Link to="#" onClick={() => window.history.back()} className="notfound-page-button">Regresar</Link>
+                </div>
             </div>
-        </div>
+        </main>
     );
 };
 
