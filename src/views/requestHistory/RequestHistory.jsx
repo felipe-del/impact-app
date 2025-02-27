691e29603f8a9d98d6f5fd3514170b0162b2a313
felipe-del <isaacfelibrenes1904@gmail.com>
2025-02-27 15:36:20 -0600

    Add RequestHistory component with banner setup
    Create a new `RequestHistory` component that includes the `RequestHistoryBanner` to display a title. This sets up the foundation for request history views.

---
+++ src/views/requestHistory/RequestHistory.jsx
@@ -0,0 +1,13 @@
+import RequestHistoryBanner from "./RequestHistoryBanner.jsx";
+
+const RequestHistory = () => {
+    return (
+        <>
+            <RequestHistoryBanner
+                title={"Historial de Solicitudes"}
+            />
+        </>
+    )
+}
+
+export default RequestHistory;
