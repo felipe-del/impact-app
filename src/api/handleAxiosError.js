8327fec8b0f7a5c645f59813e5a4e215af0febd3
Isaac Felipe Brenes Calderón <isaacfelibrenes1904@gmail.com>
2025-03-28 09:42:09 -0600

    Handle undefined or invalid responseWrapper.data gracefully
    Updated error handling to ensure `responseWrapper.data` is checked for validity before accessing its properties. Added a fallback generic error message when `responseWrapper.data` is not defined. This improves robustness and prevents potential runtime errors.

--- src/api/handleAxiosError.js
+++ src/api/handleAxiosError.js
@@ -12,12 +12,17 @@ export default function handleAxiosError(error) {
 function createErrorMessage(responseWrapper) {
     let detailMessage = "";
 
-    if (!responseWrapper.data.message) {
+    // Verifica que responseWrapper.data esté definido y sea un objeto antes de acceder a sus propiedades
+    if (responseWrapper.data && responseWrapper.data.message) {
+        detailMessage = responseWrapper.data.message;
+    } else if (responseWrapper.data) {
+        // Si no tiene message, recorre los detalles de error
         detailMessage = Object.entries(responseWrapper.data)
             .map(([field, error]) => `${field}: ${error}`)
             .join("\n");
     } else {
-        detailMessage = responseWrapper.data.message;
+        // Si responseWrapper.data no está definido, asignar un mensaje de error genérico
+        detailMessage = "Server Error. Please try again later.";
     }
 
     return `${responseWrapper.message} - ${detailMessage}`;
