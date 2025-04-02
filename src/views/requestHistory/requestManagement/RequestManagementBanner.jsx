45f638b05b2d40461cb506b98938aece5a3ac408
Isaac Felipe Brenes Calderón <isaacfelibrenes1904@gmail.com>
2025-04-02 01:37:16 -0600

    Add RequestManagementBanner component to handle banners
    This component renders a banner with a customizable title and optional action buttons. It supports a "goBack" button that navigates the user to the previous page, improving navigation usability.

---
+++ src/views/requestHistory/requestManagement/RequestManagementBanner.jsx
@@ -0,0 +1,39 @@
+import PropTypes from "prop-types";
+import { bannerStyle } from "../../../style/codeStyle.js";
+import BannerActionButton from "../../../components/button/BannerActionButton.jsx";
+import {ArrowBack} from "@mui/icons-material";
+import {useNavigate} from "react-router-dom";
+
+const RequestManagementBanner = ({
+                              title = "",
+                              visibleButtons = ["goBack"],
+                          }) => {
+
+    const styles = bannerStyle;
+    const navigate = useNavigate();
+    return (
+        <div className="export-buttons" style={styles.banner}>
+            <h3 style={styles.title}>{title}</h3>
+            <div style={styles.buttonsContainer}>
+                {visibleButtons.includes("goBack") && (
+                    <BannerActionButton
+                        onClick={() => navigate(-1)}
+                        text={"Volver"}
+                        icon={<ArrowBack />}
+                        color={"error"}
+                        style={styles.button}
+                    />
+
+                )}
+            </div>
+
+        </div>
+    );
+};
+
+RequestManagementBanner.propTypes = {
+    title: PropTypes.string,
+    visibleButtons: PropTypes.arrayOf(PropTypes.string),
+};
+
+export default RequestManagementBanner;
