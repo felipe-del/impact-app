8ea096dfc53ea3365f5b049f47088b8b91ade734
Isaac Felipe Brenes Calderón <isaacfelibrenes1904@gmail.com>
2025-04-17 22:51:33 -0600

    Add info modal and button to AllRequestBanner component
    Introduce a new "info" button to the AllRequestBanner, which opens a modal displaying detailed information about the system's unified request table. This enhances the user experience by providing additional context directly within the UI.

--- src/views/requestHistory/allRequest/AllRequestBanner.jsx
+++ src/views/requestHistory/allRequest/AllRequestBanner.jsx
@@ -1,16 +1,24 @@
 import PropTypes from "prop-types";
 import { bannerStyle } from "../../../style/codeStyle.js";
 import BannerActionButton from "../../../components/button/BannerActionButton.jsx";
-import {ArrowBack} from "@mui/icons-material";
-import {useNavigate} from "react-router-dom";
+import { ArrowBack, InfoOutlined } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import GenericInformationModal from "../../../components/popUp/GenericInformationModal/GenericInformationModal.jsx";
 
 const AllRequestBanner = ({
                               title = "",
                               visibleButtons = ["goBack"],
                           }) => {
-
     const styles = bannerStyle;
     const navigate = useNavigate();
+
+    // Estado para manejar el modal
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleOpenModal = () => setIsModalOpen(true);
+    const handleCloseModal = () => setIsModalOpen(false);
+
     return (
         <div className="export-buttons" style={styles.banner}>
             <h3 style={styles.title}>{title}</h3>
@@ -23,10 +31,26 @@ const AllRequestBanner = ({
                         color={"error"}
                         style={styles.button}
                     />
+                )}
 
+                {visibleButtons.includes("info") && (
+                    <BannerActionButton
+                        onClick={handleOpenModal}
+                        text={"Información"}
+                        icon={<InfoOutlined />}
+                        color={"primary"}
+                        style={styles.button}
+                    />
                 )}
             </div>
 
+            {/* Modal de información */}
+            <GenericInformationModal
+                open={isModalOpen}
+                onClose={handleCloseModal}
+                title="Información general"
+                description="Representa una tabla unificada que muestra todas las solicitudes de recursos del sistema, excluyendo las que están pendientes o son renovaciones. Agrupa visualmente diferentes tipos de solicitudes (activos, productos y espacios) y presenta información clave como el tipo de solicitud, el usuario, el motivo, el recurso solicitado, la fecha y el estado actual."
+                />
         </div>
     );
 };
